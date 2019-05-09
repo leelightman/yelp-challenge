ac1d8746152d035e1a8469b2786249b2f7d9c400
Lynn <leelightman@gmail.com>
2019-05-09 00:57:54 -0400

    Update yelp.js

--- yelp.js
+++ yelp.js
@@ -1,27 +1,29 @@
 var eventList = document.getElementById('eventList')
-var zipcode;
-var responseJson;
-const apicall = "https://thingproxy.freeboard.io/fetch/https://api.yelp.com/v3/events?location=";
-var url;
 var HTTPRequest;
 $(document).ready(function() {
     var loc = $('#location_input');
     var getzip = $('#getZip');
-    getzip.click(function() {
-        zipcode = loc.val();
+    getzip.click(function() { // click on event trigger
+        var zipcode = loc.val();
+        //constuct http request with user input
         constructHTTPRequest(zipcode);
         $.ajax(HTTPRequest).done(function (response) {
-            responseJson = response.events;
-            appendResults(responseJson);
-            console.log(response);
+            var responseJson = response.events;
+            console.log(responseJson);
+            appendResults(responseJson); // assemble and display event list
         });
     });
 });
 
-function constructHTTPRequest(userinput) {
-    var date = new Date().valueOf();
+function constructHTTPRequest(zipcode) {
+    var date = new Date().getTime();
     var today = Math.round(date/1000);
-    url = apicall + zipcode+ "&start_date="+today+"&sort_on=time_start&sort_by=asc&radius=50&limit=10";
+    console.log(today);
+    const thingproxy = "https://thingproxy.freeboard.io/fetch/" // thing proxy
+    const yelpcall = "https://api.yelp.com/v3/events?location="; 
+    var url = thingproxy +yelpcall+ zipcode+ "&start_date="+today+"&sort_on=time_start&sort_by=asc&radius=100&limit=10";
+    
+    // construct http request with fusion API key
     HTTPRequest = {
         "async": true,
         "crossDomain": true,
@@ -31,40 +33,44 @@ function constructHTTPRequest(userinput) {
             "Authorization": "Bearer almpIVjLwkEeGHzkLMcGFAMtaibCg83S27FXIM_QZFxwJqfBoBiI8l4ZgKI7BimOrnm86gDhOdgr_gI6KPcwjmUSmx6eZIUdox8xjrf0P-xLfARrM0XbWFcKHFbCXHYx",
             "cache-control": "no-cache",
         }
-    }
+    } 
 }
 
-function createTag(element)
-{
-    return document.createElement(element);
-
-}
-
-function append(parent, el)
-{
-    return parent.appendChild(el);
-}
-
-
-
+// list each event with details
 function appendResults(event) {
-    var eventlistall = "";
+    var eventlistall = ""; //initialize event list
     for ( i= 0; i<event.length;i++){
+        
+        //display event name
         var name ="<li class='title list-group-item'>" + event[i].name +"</li>";
+        
+        // display event cost
         var eventCost = "-";
         if(event[i].cost!=null){
-            eventCost = event[i].cost;
+            eventCost = "$"+event[i].cost;
+        } else if (event[i].is_free == true){
+            eventCost = "Free";
         }
-        cost ="<li class='list-group-item'>Cost: $" + eventCost +"</li>";
-        var time ="<li class='list-group-item'>Time: " + event[i].time_start.split('T')[0] +" to "+event[i].time_end.split('T')[0]+"</li>";
+        cost ="<li class='list-group-item'>Cost: " + eventCost +"</li>";
+        // display start time
+        var time ="<li class='list-group-item'>Start Time: " + event[i].time_start.split('T')[1].slice(0,5) +"</li>";
+        
+        // hide google api key
         var keyPartial = "AIzaSyCTyDHuF9EJa51vkw";
         var fullAddress = event[i].location.display_address;
+        
+        // construct google MAP request
         var src= "src='https://www.google.com/maps/embed/v1/place?key="+keyPartial+"NqAkwnWh4ApWqvb0o &q=" +fullAddress+"'"
-
+        
+        // retrieve google map
         var map = "<li class='collapse' id='collapse-"+i+"'><iframe height = '200' width='100%' frameborder='0' style='border:0'" + src+"allowfullscreen>" +
             "</iframe></li>"
         var address = "<li class='list-group-item'>Address: " + fullAddress +"<button type='button' class='btn btn-primary' data-target='#collapse-"+i+"' data-toggle='collapse'>Find in map</button>"+ "</li>"
+        
+        // assemble event details
         eventlistall += name+cost+time+address+map;
+        
+        // display event list
         if(eventlistall!=null){
             eventList.innerHTML = eventlistall;
         }
